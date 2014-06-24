143ecdd0e812743a1d8d233f0d2b352779c296e8
warut <warut.kmutnb@gmail.com>
2014-06-24 20:35:09 +0700

    pass argument to select interface

--- script/script1.sh
+++ script/script1.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-
+get_speed(){
+	awk '{if(l1){print  "Download:"($2-l1)/1024"kB/s",
+               "\tUpload:", ($10-l2)/1024"kB/s"} else{l1=$2; l2=$10;}}' \
+    <(grep $1 /proc/net/dev) <(sleep 1; grep $1 /proc/net/dev)
+}
 while true
 do
-     awk '{if(l1){print  "Download:"($2-l1)/1024"kB/s",
-               "\tUpload:", ($10-l2)/1024"kB/s"} else{l1=$2; l2=$10;}}' \
-    <(grep wlan0 /proc/net/dev) <(sleep 1; grep wlan0 /proc/net/dev)
+     get_speed eth0
     
 done
